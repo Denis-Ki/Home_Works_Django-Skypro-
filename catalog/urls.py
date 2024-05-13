7aead8b613316129eb72b676dabb6f3b6b7ce404
Denis Ki <duzzz@bk.ru>
2024-05-13 20:15:36 +0300

    create html in dir templates and edit views.py and urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
+from catalog.views import home
+from catalog.views import contact
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('',)
+    path("", home, name="index"),
+    path("contact/", contact, name="contact")
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def contact(request):
+    if request.method == 'POST':
+        # в переменной request хранится информация о методе, который отправлял пользователь
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(name)
+        print(phone)
+        print(message)
+    return render(request, "contacts.html")
